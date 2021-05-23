ab6197aa20282131fe752becae1ac1938dc15266
Andre Ferraz <andreferraz25384@gmail.com>
2021-05-23 15:24:31 -0300

    Create Order factory

--- elixir/exlivery/test/support/factory.ex
+++ elixir/exlivery/test/support/factory.ex
@@ -19,4 +19,13 @@ defmodule Exlivery.Factory do
       unity_price: Decimal.new("45.9")
     }
   end
+
+  def order_factory do
+    %Exlivery.Orders.Order{
+      delivery_address: "Rua dos Bobos, nº 0",
+      items: build_list(2, :item),
+      total_price: Decimal.new("91.8"),
+      user_cpf: "12345678900"
+    }
+  end
 end
