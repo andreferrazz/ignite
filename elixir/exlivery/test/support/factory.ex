480ed8e80c6326881dd6e42bc68ff65cb5525531
Andre Ferraz <andreferraz25384@gmail.com>
2021-05-23 14:55:43 -0300

    Create item factory

--- elixir/exlivery/test/support/factory.ex
+++ elixir/exlivery/test/support/factory.ex
@@ -1,15 +1,22 @@
 defmodule Exlivery.Factory do
   use ExMachina
 
-  alias Exlivery.Users.User
-
   def user_factory do
-    %User{
+    %Exlivery.Users.User{
       name: "Joao",
       cpf: "12345678900",
       email: "joao@fake.co",
       age: 21,
       address: "Rua dos Bobos, nº 0"
+    }
+  end
+
+  def item_factory do
+    %Exlivery.Orders.Item{
+      category: :pizza,
+      description: "Pizza gigante",
+      quantity: 1,
+      unity_price: Decimal.new("45.9")
     }
   end
 end
