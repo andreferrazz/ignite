8224a1e1a14bd4341cfe43fe4aa9c461cd17b611
Andre Ferraz <andreferraz25384@gmail.com>
2021-05-24 05:36:41 -0300

    Create API for create or update an User

---
+++ elixir/exlivery/lib/users/create_or_update.ex
@@ -0,0 +1,13 @@
+defmodule Exlivery.Users.CreateOrUpdate do
+  alias Exlivery.Users.Agent, as: UserAgent
+  alias Exlivery.Users.User
+
+  def call(%{address: address, name: name, cpf: cpf, age: age, email: email}) do
+    address
+    |> User.build(age, cpf, email, name)
+    |> save_user()
+  end
+
+  defp save_user({:ok, %User{} = user}), do: UserAgent.save(user)
+  defp save_user({:error, _} = error), do: error
+end
