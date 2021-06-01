0871e65d6b1342a1154aa661e8151a628b116bf0
Andre Ferraz <andreferraz25384@gmail.com>
2021-06-01 05:30:28 -0300

    Facade to create order.

--- elixir/exlivery/lib/exlivery.ex
+++ elixir/exlivery/lib/exlivery.ex
@@ -1,10 +1,14 @@
 defmodule Exlivery do
+  alias Exlivery.Orders.Agent, as: OrderAgent
+  alias Exlivery.Orders.CreateOrUpdate, as: SaveOrder
   alias Exlivery.Users.Agent, as: UserAgent
   alias Exlivery.Users.CreateOrUpdate, as: SaveUser
 
-  def start_links do
+  def start_agents do
     UserAgent.start_link(%{})
+    OrderAgent.start_link(%{})
   end
 
   defdelegate create_or_update_user(params), to: SaveUser, as: :call
+  defdelegate create_or_update_order(params), to: SaveOrder, as: :call
 end
