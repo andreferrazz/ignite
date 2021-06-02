05d45d12717fc6c4557f875a5b811daf0121c22b
Andre Ferraz <andreferraz25384@gmail.com>
2021-06-02 18:09:04 -0300

    Simplify the test case when user is found in get/1.

--- elixir/exlivery/test/users/agent_test.exs
+++ elixir/exlivery/test/users/agent_test.exs
@@ -9,7 +9,9 @@ defmodule Exlivery.Users.AgentTest do
   describe "save/1" do
     test "saves the user" do
       UserAgent.start_link(%{})
+
       user = build(:user)
+
       assert UserAgent.save(user) == :ok
     end
   end
@@ -23,28 +25,22 @@ defmodule Exlivery.Users.AgentTest do
     test "when user is found, returns an user" do
       cpf = "00000000011"
 
-      :user
-      |> build(cpf: cpf)
-      |> UserAgent.save()
+      user = build(:user, cpf: cpf)
+
+      UserAgent.save(user)
 
       result = UserAgent.get(cpf)
 
-      expected =
-        {:ok,
-         %User{
-           name: "Joao",
-           cpf: cpf,
-           email: "joao@fake.co",
-           age: 21,
-           address: "Rua dos Bobos, nº 0"
-         }}
+      expected = {:ok, user}
 
       assert result == expected
     end
 
     test "when user is not found, returns an error" do
       result = UserAgent.get("00000000011")
+
       expected = {:error, "User not found"}
+
       assert result == expected
     end
   end
