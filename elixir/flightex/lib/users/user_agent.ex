7a3c8245eb4d89112040a2d558036bd1911bc71e
Andre Ferraz <andreferraz25384@gmail.com>
2021-06-20 19:08:02 -0300

    Create Flightex.Users.UserAgent.
    This module simulate a repository of users.
    This module has three public function an one private function.
    public:
      - start_link/1 -> initiate the state of the agent
      - save/1 -> receive a user and save it
      - get/1 -> receive an id and returns the user
    private:
      - get_user/2 -> receive the current state and id and returns the user

---
+++ elixir/flightex/lib/users/user_agent.ex
@@ -0,0 +1,29 @@
+defmodule Flightex.Users.UserAgent do
+  use Agent
+
+  alias Flightex.Users.User
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%User{id: id} = user) do
+    Agent.update(__MODULE__, fn state -> Map.put(state, id, user) end)
+
+    case get(id) do
+      {:error, _reason} -> {:error, "could not save the user"}
+      {:ok, _user} -> {:ok, "created"}
+    end
+  end
+
+  def get(id) do
+    Agent.get(__MODULE__, fn state -> get_user(state, id) end)
+  end
+
+  defp get_user(state, id) do
+    case Map.get(state, id) do
+      nil -> {:error, "Could not found user #{id}"}
+      user -> {:ok, user}
+    end
+  end
+end
