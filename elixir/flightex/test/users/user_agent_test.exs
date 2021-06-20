5671927634b8f57d946fa03646080b2cfc2b9ddb
Andre Ferraz <andreferraz25384@gmail.com>
2021-06-20 19:10:37 -0300

    Create unit tests for Flightex.Users.UserAgent.
    describe save/1
    test 'when the user is save, returns an tuple with :ok and a message'
    
    describe get/1
    test 'when the user is found, returns the user'
    test 'when the user is not found, returns an error'

---
+++ elixir/flightex/test/users/user_agent_test.exs
@@ -0,0 +1,48 @@
+defmodule Flightex.Users.UserAgentTest do
+  use ExUnit.Case
+
+  alias Flightex.Users.User
+  alias Flightex.Users.UserAgent
+
+  describe "save/1" do
+    test "when the user is save, returns an tuple with :ok and a message" do
+      {:ok, user} = User.build("André", "andre@mail.com", "11111111100")
+
+      UserAgent.start_link(%{})
+
+      result = UserAgent.save(user)
+
+      expected = {:ok, "created"}
+
+      assert result == expected
+    end
+  end
+
+  describe "get/1" do
+    setup do
+      {:ok, user} = User.build("André", "andre@mail.com", "11111111100")
+
+      UserAgent.start_link(%{})
+
+      UserAgent.save(user)
+
+      {:ok, user: user}
+    end
+
+    test "when the user is found, returns the user", %{user: user} do
+      result = UserAgent.get(user.id)
+
+      expected = {:ok, user}
+
+      assert result == expected
+    end
+
+    test "when the user is not found, returns an error" do
+      result = UserAgent.get("<id>")
+
+      expected = {:error, "Could not found user <id>"}
+
+      assert result == expected
+    end
+  end
+end
