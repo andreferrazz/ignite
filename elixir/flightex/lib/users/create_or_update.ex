84cdf4e2c9c8a7e0feb74cb4a757bc3b001b246a
Andre Ferraz <andreferraz25384@gmail.com>
2021-06-22 18:17:51 -0300

    Create module Flightex.Users.CreateOrUpdate.
    Public fuction call/1 that receives a map of params and create a user if
    the id doesn't exists yet or is invalid. However, if id already exists,
    update the user.

---
+++ elixir/flightex/lib/users/create_or_update.ex
@@ -0,0 +1,31 @@
+defmodule Flightex.Users.CreateOrUpdate do
+  alias Flightex.Users.User
+  alias Flightex.Users.UserAgent
+
+  def call(%{id: id, name: _name, email: _email, cpf: _cpf} = params) do
+    id
+    |> UUID.info()
+    |> get_user_and_save(params)
+  end
+
+  defp get_user_and_save({:ok, _info}, params) do
+    UserAgent.get(params.id)
+    |> save_user(params)
+  end
+
+  defp get_user_and_save({:error, _reason} = error, params) do
+    save_user(error, params)
+  end
+
+  defp save_user({:error, _reason}, %{name: name, email: email, cpf: cpf}) do
+    name
+    |> User.build(email, cpf)
+    |> elem(1)
+    |> UserAgent.save()
+  end
+
+  defp save_user({:ok, %User{} = user}, %{name: name, email: email, cpf: cpf}) do
+    %{user | name: name, email: email, cpf: cpf}
+    |> UserAgent.save()
+  end
+end
