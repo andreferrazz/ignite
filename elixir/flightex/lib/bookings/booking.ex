9b80af7235564c6c116700d07d5481a95ca20429
Andre Ferraz <andreferraz25384@gmail.com>
2021-07-05 06:43:37 -0300

    Handle errors in Flightex.Bookings.Booking.
    In the build/4, when the user id isn't a valid UUID, returns an error.

--- elixir/flightex/lib/bookings/booking.ex
+++ elixir/flightex/lib/bookings/booking.ex
@@ -1,10 +1,17 @@
 defmodule Flightex.Bookings.Booking do
-  @keys [:id, :complete_date, :city_origin, :city_destiny, :id_usuario]
+  @keys [:id, :complete_date, :city_origin, :city_destiny, :user_id]
   @enforce_keys @keys
 
   defstruct @keys
 
-  def build(complete_date, city_origin, city_destiny, id_usuario) do
+  def build(complete_date, city_origin, city_destiny, user_id) do
+    case UUID.info(user_id) do
+      {:ok, _info} -> build_booking(complete_date, city_origin, city_destiny, user_id)
+      {:error, _reason} -> {:error, "the given user id is invalid"}
+    end
+  end
+
+  defp build_booking(complete_date, city_origin, city_destiny, user_id) do
     {
       :ok,
       %__MODULE__{
@@ -12,7 +19,7 @@ defmodule Flightex.Bookings.Booking do
         complete_date: complete_date,
         city_origin: city_origin,
         city_destiny: city_destiny,
-        id_usuario: id_usuario
+        user_id: user_id
       }
     }
   end
