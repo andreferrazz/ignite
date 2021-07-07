5b0bcef87994920cd8e34230ca00b60022c11307
Andre Ferraz <andreferraz25384@gmail.com>
2021-07-07 05:00:31 -0300

    Create Flightex.Bookings.BookingAgent.
    public function start_link/1 -> Initiate agent state.
    public function save/1 -> Save the booking.
    public function get/1 -> Find a saved booking by id.
    private function save_booking/1.
    private function get_booking/1.

---
+++ elixir/flightex/lib/bookings/booking_agent.ex
@@ -0,0 +1,33 @@
+defmodule Flightex.Bookings.BookingAgent do
+  use Agent
+
+  alias Flightex.Bookings.Booking
+  alias Flightex.Users.UserAgent
+
+  def start_link(_initial_state) do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
+  end
+
+  def save(%Booking{user_id: user_id} = booking) do
+    case UserAgent.get(user_id) do
+      {:ok, _user_id} -> save_booking(booking)
+      error -> error
+    end
+  end
+
+  def get(id) do
+    Agent.get(__MODULE__, fn state -> get_booking(state, id) end)
+  end
+
+  defp save_booking(%Booking{id: id} = booking) do
+    Agent.update(__MODULE__, fn state -> Map.put(state, id, booking) end)
+    {:ok, id}
+  end
+
+  defp get_booking(state, id) do
+    case Map.get(state, id) do
+      nil -> {:error, "Could not found booking #{id}"}
+      booking -> {:ok, booking}
+    end
+  end
+end
