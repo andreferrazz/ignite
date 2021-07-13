30aafdca646f9107a839e81db2dec4913342ad0f
Andre Ferraz <andreferraz25384@gmail.com>
2021-07-12 23:40:45 -0300

    New test in Flightex.Bookings.BookingAgentTest.
    Describe get_all/0.
    Test 'when called, should return a map: id -> booking'.

--- elixir/flightex/test/bookings/booking_agent_test.exs
+++ elixir/flightex/test/bookings/booking_agent_test.exs
@@ -69,4 +69,51 @@ defmodule Flightex.Bookings.BookingAgentTest do
       assert result == expected
     end
   end
+
+  describe "get_all/0" do
+    test "when called, should return a map: id -> booking" do
+      Flightex.start_agents()
+
+      {:ok, user_id} = Flightex.save_user(%{id: nil, name: "Joao", email: "j@w", cpf: "12345678"})
+
+      {:ok, booking_id_1} =
+        Flightex.save_booking(%{
+          id: nil,
+          complete_date: ~N[2021-07-20 18:00:00],
+          city_origin: "Belo Horizonte",
+          city_destiny: "New York",
+          user_id: user_id
+        })
+
+      {:ok, booking_id_2} =
+        Flightex.save_booking(%{
+          id: nil,
+          complete_date: ~N[2021-08-01 15:00:00],
+          city_origin: "Rome",
+          city_destiny: "London",
+          user_id: user_id
+        })
+
+      result = BookingAgent.get_all()
+
+      expected = %{
+        booking_id_1 => %Flightex.Bookings.Booking{
+          city_destiny: "New York",
+          city_origin: "Belo Horizonte",
+          complete_date: ~N[2021-07-20 18:00:00],
+          id: booking_id_1,
+          user_id: user_id
+        },
+        booking_id_2 => %Flightex.Bookings.Booking{
+          city_destiny: "London",
+          city_origin: "Rome",
+          complete_date: ~N[2021-08-01 15:00:00],
+          id: booking_id_2,
+          user_id: user_id
+        }
+      }
+
+      assert result == expected
+    end
+  end
 end
