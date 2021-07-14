8398ee70cbde60cd7b641510fda0f9515c7cddef
Andre Ferraz <andreferraz25384@gmail.com>
2021-07-14 04:49:41 -0300

    Ajust Flightex.Bookings.ReportTest.
    Change the test to match the actual report generation.

--- elixir/flightex/test/bookings/report_test.exs
+++ elixir/flightex/test/bookings/report_test.exs
@@ -4,13 +4,16 @@ defmodule Flightex.Bookings.ReportTest do
   alias Flightex.Bookings.BookingAgent
   alias Flightex.Bookings.Report
 
-  describe "generate/1" do
+  describe "generate/3" do
     test "when the operation succeed, return :ok" do
       BookingAgent.start_link(%{})
 
-      result = Report.generate("report_test.csv")
+      from_date = ~N[2021-08-03 00:00:00]
+      to_date = ~N[2021-08-30 23:59:00]
 
-      expected = :ok
+      result = Report.generate("report_test.csv", from_date, to_date)
+
+      expected = {:ok, "Report generated successfully"}
 
       assert result == expected
     end
